bc675711f73dde5464acaf16faf87e8afecbe534
eijintao <1747486530@qq.com>
2022-05-01 00:02:13 +0800

    雪花算法

---
+++ springbootdemo/src/main/java/com/projectdemo/springbootdemo/entity/Testtest.java
@@ -0,0 +1,19 @@
+package com.projectdemo.springbootdemo.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * asus 梅锦涛
+ * 2022/4/29
+ *
+ * @author mjt
+ */
+@Data
+@ToString
+public class Testtest {
+
+    private String test1;
+    private String test2;
+
+}
